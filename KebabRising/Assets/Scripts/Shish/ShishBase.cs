53ba65cd7a21ff4641244b02b82ee3dabd02b5f5
mhmtckr27 <l1116023@std.yildiz.edu.tr>
2019-08-05 01:21:11 +0300

    Added ShishBase-ShishType1 Scripts

---
+++ KebabRising/Assets/Scripts/Shish/ShishBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ShishBase : MonoBehaviour
+{
+	protected int maxNumberOfSlots { get; set; }
+	//protected Slot[] Slots { get; set; }
+	protected int maxNumberOfBounces { get; set; }
+	protected Vector2 moveSpeed { get; set; }
+	protected bool canBreakGlass { get; set; }
+	protected bool canBurn { get; set; }
+}
