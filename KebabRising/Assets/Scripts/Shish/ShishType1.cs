ef447c238945080aba2ed24fa90faf1a0487db96
fzlink <fazlibozatay@gmail.com>
2019-08-09 22:40:01 +0300

    Tests on Shish Rotation

--- KebabRising/Assets/Scripts/Shish/ShishType1.cs
+++ KebabRising/Assets/Scripts/Shish/ShishType1.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,8 @@ public class ShishType1 : ShishBase
 	[SerializeField] private bool canBreakGlass;
 	[SerializeField] private bool canBurn;
 
+
+    private bool isClicked;
 	private int remainingBounces;
 	[Header("Main Properties")][Space]
 	[SerializeField] private Sprite burntShishSprite;
@@ -24,11 +27,53 @@ public class ShishType1 : ShishBase
 
 	void Update()
 	{
-		Debug.Log(canBurn+" "+base.CanBurn);
+		//Debug.Log(canBurn+" "+base.CanBurn);
+        TouchControl();
+        TouchDrag();
 	}
 
-	//Initializes base class properties when object created.
-	void InitializeBaseProperties()
+    private void TouchDrag()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //gameObject.transform.rotation = Vector3.Angle(Input.mousePosition, gameObject.transform.position);
+
+            //gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(0, 0, 1), Input.mousePosition * -1);
+            Debug.Log(Input.mousePosition);
+            //Debug.Log(gameObject.transform.rotation);
+
+            Vector3 mousePos = Input.mousePosition;
+
+            //Mathf.Clamp(mousePos.y, 55, 1000);
+            Vector3 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - Camera.main.transform.position.z));
+
+            
+            gameObject.transform.eulerAngles = new Vector3(0,0,Mathf.Clamp(    Mathf.Atan2(  (screenPos.y - transform.position.y), (screenPos.x - transform.position.x)   ), 1.57f,3.14f  )   * Mathf.Rad2Deg);
+            
+        }
+    }
+
+    private void TouchControl()
+    {
+    //    if (Input.GetMouseButtonDown(0))
+    //        isClicked = true;
+
+    //    if (isClicked)
+    //    {
+    //        //Debug.Log("HELEOLOELOEL");
+    //        Vector2 angle = new Vector2(-1, 1);
+    //        float moveSpeedX = angle.x * 3;
+    //        float moveSpeedY = angle.y * 3;
+
+    //        gameObject.transform.position += new Vector3(moveSpeedX, moveSpeedY, 0) * Time.fixedDeltaTime;
+
+    //    }
+    }
+
+    //Initializes base class properties when object created.
+    void InitializeBaseProperties()
 	{
 		base.MaxNumberOfSlots = maxNumberOfSlots;
 		base.MaxNumberOfBounces = maxNumberOfBounces;
@@ -46,14 +91,14 @@ public class ShishType1 : ShishBase
 	{
 		GetComponent<SpriteRenderer>().sprite = burntShishSprite;
 		
-		//burn related things will go there(animation, sound, etc.)
+		//burn related things will go here(animation, sound, etc.)
 	}
 
 	private void DestroyShish()
 	{
 		Destroy(gameObject, 0f);
 
-		//shish destruction related things will go there(animation, sound, etc.)
+		//shish destruction related things will go here(animation, sound, etc.)
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
